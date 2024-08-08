8aa9d0106b6933ddd219800b39fe7ae3de7a4854
biego <biegoe.lozano@gmail.com>
2024-08-08 18:39:26 -0500

    chore(modules): add 'apps' directory to system path

--- api/settings.py
+++ api/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Agrega la carpeta 'apps' al sistema de módulos
+sys.path.insert(0, str(BASE_DIR / "apps"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
