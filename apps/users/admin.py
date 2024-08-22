3a852523131df7bf569f5a1a5886237d4d996e9a
biego <biegoe.lozano@gmail.com>
2024-08-22 02:44:22 -0500

    feat(admin): customize CustomUser admin interface with fieldsets, readonly settings and list display

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        ('Acceso y Seguridad', {'fields': ('username', 'password', 'last_login')}),
+        ('Datos Personales', {'fields': ('dni', 'first_name', 'last_name', 'email', 'corporate_phone', 'corporate_device_imei', 'supervisor')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas Importantes', {'fields': ('employment_start_date', 'employment_end_date', 'created_at')}),
+    )
+    readonly_fields = ('created_at',)
+
+    # Campos visibles en la lista de usuarios
+    list_display = UserAdmin.list_display + ('is_active',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
