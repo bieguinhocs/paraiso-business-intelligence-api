cbc0df95bcd16b12278a18a29c42398f1dab22e2
biego <biegoe.lozano@gmail.com>
2024-08-22 04:10:01 -0500

    feat(api): implement CustomUserViewSet to manage endpoints with full CRUD support

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-# Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CustomUserSerializer
