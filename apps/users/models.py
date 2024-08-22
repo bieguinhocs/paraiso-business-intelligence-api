779b9cfc867e59854baa6df2cc927365b3c6c5d7
biego <biegoe.lozano@gmail.com>
2024-08-22 02:55:13 -0500

    refactor(users): omit date_joined in favor of created_at in CustomUser model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
     dni = models.CharField(max_length=255, unique=True, verbose_name='DNI')
     corporate_phone = models.CharField(max_length=255, blank=True, null=True, verbose_name='Número de teléfono corporativo')
@@ -11,6 +10,8 @@ class CustomUser(AbstractUser):
     #address = models.ForeignKey('locations.Address', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Dirección')
     supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Coordinador')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+
+    # Campo de AbstractUser omitido y reemplazado por 'created_at'
     date_joined = models.DateTimeField(null=True, blank=True, editable=False)
     
     class Meta:
