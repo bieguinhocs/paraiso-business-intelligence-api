2511ad5a183c884c0cc5eadf2d964252c790b08c
biego <biegoe.lozano@gmail.com>
2024-08-24 01:49:14 -0500

    feat(api): add viewsets for address-related models to manage endpoints with full CRUD support

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import AddressDepartment, AddressCity, AddressZoneGroup, AddressDistrict, Address
+from .serializers import (
+    AddressDepartmentSerializer,
+    AddressCitySerializer,
+    AddressZoneGroupSerializer,
+    AddressDistrictSerializer,
+    AddressSerializer
+)
 
-# Create your views here.
+class AddressDepartmentViewSet(viewsets.ModelViewSet):
+    queryset = AddressDepartment.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressDepartmentSerializer
+
+class AddressCityViewSet(viewsets.ModelViewSet):
+    queryset = AddressCity.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressCitySerializer
+
+class AddressZoneGroupViewSet(viewsets.ModelViewSet):
+    queryset = AddressZoneGroup.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressZoneGroupSerializer
+
+class AddressDistrictViewSet(viewsets.ModelViewSet):
+    queryset = AddressDistrict.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressDistrictSerializer
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AddressSerializer
