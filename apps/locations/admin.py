6fcb674473657fa68e118d8c2325c2ccc3045f49
biego <biegoe.lozano@gmail.com>
2024-08-24 03:21:47 -0500

    feat(admin): register address-related models with custom admin configurations

--- apps/locations/admin.py
+++ apps/locations/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import AddressDepartment, AddressCity, AddressZoneGroup, AddressDistrict, Address
 
-# Register your models here.
+@admin.register(AddressDepartment)
+class AddressDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(AddressCity)
+class AddressCityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department',)
+    search_fields = ('name',)
+    list_filter = ('department',)
+
+@admin.register(AddressZoneGroup)
+class AddressZoneGroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(AddressDistrict)
+class AddressDistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'zone_group',)
+    search_fields = ('name',)
+    list_filter = ('city', 'zone_group')
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'district',)
+    search_fields = ('name',)
+    list_filter = ('district',)
