4c4d0031d1755909677227058d5079cd5d2e731e
biego <biegoe.lozano@gmail.com>
2024-08-28 01:35:37 -0500

    chore(urls): use custom admin site in project routing

--- api/urls.py
+++ api/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .admin import custom_admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('admin/', custom_admin_site.urls),
     path('api/', include('users.urls')),
     path('api/locations/', include('locations.urls')),
 ]
