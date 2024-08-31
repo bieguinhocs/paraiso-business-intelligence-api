56a33c6001c92cdb06f8355b8042dd7e4690518e
biego <biegoe.lozano@gmail.com>
2024-08-31 16:53:12 -0500

    refactor(locations): use gettext_lazy for translating fields in address-related models

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -1,62 +1,63 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class AddressDepartment(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Nombre')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    name = models.CharField(_('name'), max_length=255, unique=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Departamento'
-        verbose_name_plural = '1. Departamentos'
+        verbose_name = _('department')
+        verbose_name_plural = _('departments')
 
     def __str__(self):
         return self.name
 
 class AddressCity(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Nombre')
-    department = models.ForeignKey(AddressDepartment, on_delete=models.CASCADE, verbose_name='Departamento')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    name = models.CharField(_('name'), max_length=255, unique=True)
+    department = models.ForeignKey(AddressDepartment, on_delete=models.CASCADE, verbose_name=_('department'))
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Ciudad'
-        verbose_name_plural = '2. Ciudades'
+        verbose_name = _('city')
+        verbose_name_plural = _('cities')
 
     def __str__(self):
         return self.name
 
 class AddressZoneGroup(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Nombre')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    name = models.CharField(_('name'), max_length=255, unique=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Grupo Zonal'
-        verbose_name_plural = '3. Grupos Zonales'
+        verbose_name = _('zonal group')
+        verbose_name_plural = _('zonal groups')
 
     def __str__(self):
         return self.name
 
 class AddressDistrict(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Nombre')
-    city = models.ForeignKey(AddressCity, on_delete=models.CASCADE, verbose_name='Ciudad')
-    zone_group = models.ForeignKey(AddressZoneGroup, on_delete=models.CASCADE, verbose_name='Grupo zonal')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    name = models.CharField(_('name'), max_length=255)
+    city = models.ForeignKey(AddressCity, on_delete=models.CASCADE, verbose_name=_('city'))
+    zone_group = models.ForeignKey(AddressZoneGroup, on_delete=models.CASCADE, verbose_name='zonal group')
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Distrito'
-        verbose_name_plural = '4. Distritos'
+        verbose_name = _('district')
+        verbose_name_plural = _('districts')
 
     def __str__(self):
         return self.name
 
 class Address(models.Model):
-    name = models.CharField(max_length=255, blank=True, null=True, verbose_name='Nombre')
-    location = models.CharField(max_length=255, blank=True, null=True, verbose_name='Ubicación')
-    district = models.ForeignKey(AddressDistrict, on_delete=models.CASCADE, verbose_name='Distrito')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    name = models.CharField(_('name'), max_length=255, blank=True, null=True)
+    location = models.CharField(_('location'), max_length=255, blank=True, null=True)
+    district = models.ForeignKey(AddressDistrict, on_delete=models.CASCADE, verbose_name='district')
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Dirección'
-        verbose_name_plural = '5. Direcciones'
+        verbose_name = _('address')
+        verbose_name_plural = _('addresses')
 
     def __str__(self):
         return self.name or self.location
- 
+    
