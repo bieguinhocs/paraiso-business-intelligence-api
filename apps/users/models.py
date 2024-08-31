ef1270660135a80142de7fd54b92266aca7c546a
biego <biegoe.lozano@gmail.com>
2024-08-31 16:51:43 -0500

    refactor(users): use gettext_lazy for translating fields in CustomUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,22 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
-    dni = models.CharField(max_length=255, unique=True, verbose_name='DNI')
-    corporate_phone = models.CharField(max_length=255, blank=True, null=True, verbose_name='Número de teléfono corporativo')
-    corporate_device_imei = models.CharField(max_length=255, blank=True, null=True, verbose_name='IMEI de teléfono corporativo')
-    employment_start_date = models.DateTimeField(blank=True, null=True, verbose_name='Inicio de empleo')
-    employment_end_date = models.DateTimeField(blank=True, null=True, verbose_name='Fin de empleo')
+    dni = models.CharField(_('DNI'), max_length=255, unique=True)
+    corporate_phone = models.CharField(_('corporate phone number'), max_length=255, blank=True, null=True)
+    corporate_device_imei = models.CharField(_('corporate device imei'), max_length=255, blank=True, null=True)
+    employment_start_date = models.DateTimeField(_('employment start'), blank=True, null=True)
+    employment_end_date = models.DateTimeField(_('employment end'), blank=True, null=True)
     #address = models.ForeignKey('locations.Address', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Dirección')
-    supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Coordinador')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación de registro')
+    supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('coordinator'))
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
 
     # Campo de AbstractUser omitido y reemplazado por 'created_at'
     date_joined = models.DateTimeField(null=True, blank=True, editable=False)
     
     class Meta:
-        verbose_name = 'Usuario'
-        verbose_name_plural = 'Usuarios'
-
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username})"
