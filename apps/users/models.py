f65eddefc32b4a0ab9f7f94691a841bc3bbe0499
biego <biegoe.lozano@gmail.com>
2024-08-31 19:58:23 -0500

    refactor(users): update CustomUser with strict field validation and length constraints

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator
 
 class CustomUser(AbstractUser):
-    dni = models.CharField(_('DNI'), max_length=255, unique=True)
-    corporate_phone = models.CharField(_('corporate phone number'), max_length=255, blank=True, null=True)
-    corporate_device_imei = models.CharField(_('corporate device imei'), max_length=255, blank=True, null=True)
+    dni = models.CharField(_('DNI'), max_length=8, validators=[MinLengthValidator(8)], unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=False)
+    last_name = models.CharField(_('last name'), max_length=150, blank=False)
+    corporate_phone = models.CharField(_('corporate phone number'), max_length=9, validators=[MinLengthValidator(9)], blank=True, null=True)
+    corporate_device_imei = models.CharField(_('corporate device imei'), max_length=15, validators=[MinLengthValidator(15)], blank=True, null=True)
     employment_start_date = models.DateTimeField(_('employment start'), blank=True, null=True)
     employment_end_date = models.DateTimeField(_('employment end'), blank=True, null=True)
     #address = models.ForeignKey('locations.Address', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Dirección')
@@ -18,6 +21,6 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-    
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username})"
