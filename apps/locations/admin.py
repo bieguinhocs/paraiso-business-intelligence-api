8aad205e266546a86f8fe581633ae8b5050789a2
biego <biegoe.lozano@gmail.com>
2024-09-02 02:55:28 -0500

    chore(admin): add display decorators and fieldsets for address-related models

--- apps/locations/admin.py
+++ apps/locations/admin.py
@@ -1,127 +1,254 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from .models import AddressDepartment, AddressCity, AddressZoneGroup, AddressDistrict, Address
-from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
-
-# Filtro por Distrito en AddressAdmin
-class DistrictFilter(admin.SimpleListFilter):
-    title = 'Distrito'
-    parameter_name = 'district'
-
-    def lookups(self, request, model_admin):
-        districts = AddressDistrict.objects.all()
-        return [(d.id, d.name) for d in districts]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(district__id=self.value())
-        return queryset
-
-# Filtro por Ciudad en AddressAdmin (a través de District)
-class CityFilterForAddress(admin.SimpleListFilter):
-    title = 'Ciudad'
-    parameter_name = 'city'
-
-    def lookups(self, request, model_admin):
-        cities = AddressCity.objects.all()
-        return [(c.id, c.name) for c in cities]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(district__city__id=self.value())
-        return queryset
-
-# Filtro por Departamento en AddressAdmin (a través de City y District)
-class DepartmentFilterForAddress(admin.SimpleListFilter):
-    title = 'Departamento'
-    parameter_name = 'department'
-
-    def lookups(self, request, model_admin):
-        departments = AddressDepartment.objects.all()
-        return [(d.id, d.name) for d in departments]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(district__city__department__id=self.value())
-        return queryset
-
-# Filtro por Ciudad en AddressDistrictAdmin
-class CityFilterForDistrict(admin.SimpleListFilter):
-    title = 'Ciudad'
-    parameter_name = 'city'
-
-    def lookups(self, request, model_admin):
-        cities = AddressCity.objects.all()
-        return [(c.id, c.name) for c in cities]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(city__id=self.value())
-        return queryset
-
-# Filtro por Departamento en AddressCityAdmin
-class DepartmentFilterForCity(admin.SimpleListFilter):
-    title = 'Departamento'
-    parameter_name = 'department'
-
-    def lookups(self, request, model_admin):
-        departments = AddressDepartment.objects.all()
-        return [(d.id, d.name) for d in departments]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(department__id=self.value())
-        return queryset
+from .models import AddressDepartment, AddressCity, AddressZonalGroup, AddressDistrict, Address
+from django.utils.translation import gettext_lazy as _
+from unfold.decorators import display
 
 @admin.register(AddressDepartment)
 class AddressDepartmentAdmin(ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    list_filter = ('created_at',)
-
-@admin.register(AddressCity)
-class AddressCityAdmin(ModelAdmin):
-    list_display = ('name', 'department',)
-    search_fields = ('name',)
-    list_filter = (DepartmentFilterForCity, 'created_at',)
-
-@admin.register(AddressZoneGroup)
-class AddressZoneGroupAdmin(ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name', 'created_at',)
-    list_filter = ('created_at',)
-
-@admin.register(AddressDistrict)
-class AddressDistrictAdmin(ModelAdmin):
-    list_display = ('name', 'city', 'get_department', 'zone_group',)
-    search_fields = ('name',)
-    list_filter = ('city', 'city__department', 'zone_group', 'created_at',)
-
-    def get_department(self, obj):
-        return obj.city.department.name
-    get_department.short_description = 'Departamento'
-
-@admin.register(Address)
-class AddressAdmin(ModelAdmin):
-    list_display = ('name', 'district', 'get_city', 'get_department', 'get_zone_group',)
-    search_fields = ('name',)
-    #list_filter = (DistrictFilter, CityFilterForAddress, DepartmentFilterForAddress, 'created_at',)
-    #list_filter = ('district', 'district__city', 'district__city__department')
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
     list_filter = (
-        ('district', RelatedDropdownFilter),  # Filtrar por distrito con un menú desplegable
-        ('district__city', RelatedDropdownFilter),  # Filtrar por ciudad a través del distrito
-        ('district__city__department', RelatedDropdownFilter),  # Filtrar por departamento a través de ciudad y distrito
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
         'created_at',
     )
 
-    def get_city(self, obj):
-        return obj.district.city.name
-    get_city.short_description = 'Ciudad'
+    @display(description=_('Created'))
+    def display_created(self, instance: AddressDepartment):
+        return instance.created_at
 
-    def get_department(self, obj):
-        return obj.district.city.department.name
-    get_department.short_description = 'Departamento'
+@admin.register(AddressCity)
+class AddressCityAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'department',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'department',
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                    'department',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
 
-    def get_zone_group(self, obj):
-        return obj.district.zone_group.name
-    get_zone_group.short_description = 'Grupo Zonal'
+    @display(description=_('Created'))
+    def display_created(self, instance: AddressCity):
+        return instance.created_at
+
+@admin.register(AddressZonalGroup)
+class AddressZonalGroupAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+        'created_at',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: AddressZonalGroup):
+        return instance.created_at
+
+@admin.register(AddressDistrict)
+class AddressDistrictAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'city',
+        'display_department',
+        'zonal_group',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'city',
+        'city__department',
+        'zonal_group',
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                    'city',
+                    'zonal_group',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Department'))
+    def display_department(self, instance: AddressDistrict):
+        return instance.city.department
+
+    @display(description=_('Created'))
+    def display_created(self, instance: AddressDistrict):
+        return instance.created_at
+
+@admin.register(Address)
+class AddressAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'district',
+        'display_city',
+        'display_department',
+        'display_zonal_group',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'district',
+        'district__city',
+        'district__city__department',
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                    'district',
+                    #'district__city',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Location'),
+            {
+                'fields': (
+                    (
+                        'latitude',
+                        'longitude',
+                    ),  
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: Address):
+        return instance.created_at
+
+    @display(description=_('City'))
+    def display_city(self, instance: Address):
+        return instance.district.city
+    
+    @display(description=_('Department'))
+    def display_department(self, instance: Address):
+        return instance.district.city.department
+    
+    @display(description=_('Zonal group'))
+    def display_zonal_group(self, instance: Address):
+        return instance.district.zonal_group
