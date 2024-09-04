c317819c815fff70c15dde85f66619b4a6b9a3c5
biego <biegoe.lozano@gmail.com>
2024-09-04 01:21:01 -0500

    chore(users): update verbose_name

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-    verbose_name = _('Users')
+    verbose_name = _('Users management')
     
