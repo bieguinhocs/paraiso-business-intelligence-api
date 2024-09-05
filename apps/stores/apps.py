022098f4b33fe4e8209d724ef6c9fa839803c6b6
biego <biegoe.lozano@gmail.com>
2024-09-04 20:01:00 -0500

    chore(stores): add verbose_name to stores app configuration

--- apps/stores/apps.py
+++ apps/stores/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class StoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stores'
+    verbose_name = _('Stores')
