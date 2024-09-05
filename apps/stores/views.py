4fb84581e04c89f74c71195675a9db3e2141335d
biego <biegoe.lozano@gmail.com>
2024-09-04 20:30:38 -0500

    feat(api): add viewsets for store-related models to manage endpoints with full CRUD support

--- apps/stores/views.py
+++ apps/stores/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import (
+    StoreChannel,
+    StoreRetail,
+    StoreCoverage,
+    Store
+)
+from .serializers import (
+    StoreChannelSerializer,
+    StoreRetailSerializer,
+    StoreCoverageSerializer,StoreSerializer
+)
 
-# Create your views here.
+class StoreChannelViewSet(viewsets.ModelViewSet):
+    queryset = StoreChannel.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StoreChannelSerializer
+
+class StoreRetailViewSet(viewsets.ModelViewSet):
+    queryset = StoreRetail.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StoreRetailSerializer
+
+class StoreCoverageViewSet(viewsets.ModelViewSet):
+    queryset = StoreCoverage.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StoreCoverageSerializer
+
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StoreSerializer
