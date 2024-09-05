acfc6a5249fd1f598c3f3c06cf195d0cf6fa6a9a
biego <biegoe.lozano@gmail.com>
2024-09-04 19:24:31 -0500

    feat(users): add address field to CustomUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -28,7 +28,7 @@ class CustomUser(AbstractUser):
     corporate_device_imei = models.CharField(_('corporate device imei'), max_length=15, validators=[MinLengthValidator(15)], blank=True, null=True)
     employment_start_date = models.DateTimeField(_('employment start'), blank=True, null=True)
     employment_end_date = models.DateTimeField(_('employment end'), blank=True, null=True)
-    #address = models.ForeignKey('locations.Address', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Dirección')
+    address = models.ForeignKey('locations.Address', on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('address'))
     supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('coordinator'))
     created_at = models.DateTimeField(_("created at"), auto_now_add=True)
 
