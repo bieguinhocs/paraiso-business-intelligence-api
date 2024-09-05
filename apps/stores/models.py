a3a0e5c815eeddd3b923e5a31213260e4bbd0834
biego <biegoe.lozano@gmail.com>
2024-09-04 23:52:32 -0500

    refactor(stores): add is_covered boolean field and remove StoreCoverage from models

--- apps/stores/models.py
+++ apps/stores/models.py
@@ -8,8 +8,8 @@ class StoreChannel(models.Model):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = _('store channel')
-        verbose_name_plural = _('store channels')
+        verbose_name = _('channel')
+        verbose_name_plural = _('channels')
 
     def __str__(self):
         return self.name
@@ -22,19 +22,8 @@ class StoreRetail(models.Model):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
-        verbose_name = _('store retail')
-        verbose_name_plural = _('store retails')
-
-    def __str__(self):
-        return self.name
-
-class StoreCoverage(models.Model):
-    name = models.CharField(_('name'), max_length=255, unique=True)
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
-
-    class Meta:
-        verbose_name = _('store coverage')
-        verbose_name_plural = _('store coverages')
+        verbose_name = _('retail')
+        verbose_name_plural = _('retails')
 
     def __str__(self):
         return self.name
@@ -43,10 +32,10 @@ class Store(models.Model):
     code = models.CharField(_('code'), max_length=100, unique=True, blank=True, null=True)
     name = models.CharField(_('name'), max_length=255, unique=True)
     sellout_name = models.CharField(_('sellout name'), max_length=255, blank=True, null=True)
-    address = models.ForeignKey('locations.Address', on_delete=models.CASCADE, verbose_name=_('address'))
+    address = models.ForeignKey('locations.Address', on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('address'))
     coordinator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='coordinator_stores', verbose_name=_('coordinator'))
     retail = models.ForeignKey(StoreRetail, on_delete=models.CASCADE, verbose_name=_('retail'))
-    coverage = models.ForeignKey(StoreCoverage, on_delete=models.CASCADE, verbose_name=_('coverage'))
+    is_covered = models.BooleanField(default=False, verbose_name=_('coverage'))
     promoters = models.ManyToManyField(get_user_model(), related_name='promoter_stores', verbose_name=_('promoters'))
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
