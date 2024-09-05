05a0c32d572d9b57d7945800f344ad30e76ffb40
biego <biegoe.lozano@gmail.com>
2024-09-04 20:43:23 -0500

    fead(admin): add StoreChannelAdmin with customization

--- apps/stores/admin.py
+++ apps/stores/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import (
+    StoreChannel,
+    StoreRetail,
+    StoreCoverage,
+    Store
+)
+from django.utils.translation import gettext_lazy as _
+from unfold.decorators import display
 
-# Register your models here.
+@admin.register(StoreChannel)
+class StoreChannelAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'name',
+                    'description',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                    'description',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: StoreChannel):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('name')
+        return queryset, use_distinct
