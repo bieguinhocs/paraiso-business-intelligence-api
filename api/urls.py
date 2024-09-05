b8af66715cfa83df72dd11c9e9d15c60be3f4498
biego <biegoe.lozano@gmail.com>
2024-09-04 20:32:05 -0500

    feat(api): include stores app URLs in project routing

--- api/urls.py
+++ api/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/locations/', include('locations.urls')),
+    path('api/stores/', include('stores.urls')), 
 ]
