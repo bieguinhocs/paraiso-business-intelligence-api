c51aa0157110e56f6982fd91ba56b5a3e4faf839
biego <biegoe.lozano@gmail.com>
2024-09-06 12:56:56 -0500

    fead(admin): add ProductGroupAdmin with customization

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import (
+    ProductGroup,
+    ProductFamily,
+    ProductLine,
+    ProductBrand,
+    Product
+)
+from django.utils.translation import gettext_lazy as _
+from unfold.decorators import display
+from django.templatetags.static import static
 
-# Register your models here.
+@admin.register(ProductGroup)
+class ProductGroupAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: ProductGroup):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('name')
+        return queryset, use_distinct
+    
