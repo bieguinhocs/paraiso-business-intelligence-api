3d573a3d6cb90a7fcde1495f9eb0565fe2a63307
biego <biegoe.lozano@gmail.com>
2024-09-06 12:40:43 -0500

    feat(api): add viewsets for product-related models to manage endpoints with full CRUD support

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import (
+    ProductGroup,
+    ProductFamily,
+    ProductLine,
+    ProductBrand,
+    Product
+)
+from .serializers import (
+    ProductGroupSerializer,
+    ProductFamilySerializer,
+    ProductLineSerializer,
+    ProductBrandSerializer,
+    ProductSerializer,
+)
 
-# Create your views here.
+class ProductGroupViewSet(viewsets.ModelViewSet):
+    queryset = ProductGroup.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductGroupSerializer
+
+class ProductFamilyViewSet(viewsets.ModelViewSet):
+    queryset = ProductFamily.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductFamilySerializer
+
+class ProductLineViewSet(viewsets.ModelViewSet):
+    queryset = ProductLine.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductLineSerializer
+
+class ProductBrandViewSet(viewsets.ModelViewSet):
+    queryset = ProductBrand.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductBrandSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
