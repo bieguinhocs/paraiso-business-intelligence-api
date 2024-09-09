63f16db7811b0d4d72f6ebdd8accf309f1a80d33
biego <biegoe.lozano@gmail.com>
2024-09-09 01:32:48 -0500

    feat(products): add inlines, display methods and new Size and Color Admins

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -5,14 +5,26 @@ from .models import (
     ProductFamily,
     ProductBrand,
     ProductLine,
+    ProductSize,
+    ProductColor,
     Product
 )
 from django.utils.translation import gettext_lazy as _
 from unfold.decorators import display
 from django.templatetags.static import static
+from unfold.admin import TabularInline
+
+class FamilyInline(TabularInline):
+    model = ProductFamily
+    fields = ['code', 'name',]
+    show_change_link = True
+    can_delete = True
+    tab = True
+    extra = 0
 
 @admin.register(ProductGroup)
 class ProductGroupAdmin(ModelAdmin):
+    inlines = [FamilyInline,]
     add_fieldsets = (
         (
             _('Overview'), 
@@ -130,11 +142,22 @@ class ProductFamilyAdmin(ModelAdmin):
     
     def get_search_results(self, request, queryset, search_term):
         queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        queryset = queryset.order_by('name')
+        queryset = queryset.order_by('group__name', 'name',)
+        for family in queryset:
+            family.name = f"{family.group.name} - {family.name}"
         return queryset, use_distinct
+
+class LineInline(TabularInline):
+    model = ProductLine
+    fields = ['name',]
+    show_change_link = True
+    can_delete = True
+    tab = True
+    extra = 0
 
 @admin.register(ProductBrand)
 class ProductBrandAdmin(ModelAdmin):
+    inlines = [LineInline,]
     add_fieldsets = (
         (
             _('Overview'), 
@@ -148,8 +171,7 @@ class ProductBrandAdmin(ModelAdmin):
         ),
     )
     list_display = (
-        'code',
-        'name',
+        'display_brand_header',
         'display_created',
     )
     search_fields = (
@@ -184,6 +206,24 @@ class ProductBrandAdmin(ModelAdmin):
         'created_at',
     )
 
+    @display(description=_('Name'), header=True)
+    def display_brand_header(self, instance: ProductBrand):
+        """
+        Muestra la marca en la primera línea, el codigo en la segunda,
+        y un avatar en un círculo.
+        """
+        image_path = f"images/brands/{instance.name.lower()}.jpg"
+        return [
+            instance.name,
+            instance.code,
+            None,
+            {
+                "path": static(image_path),
+                "squared": False,
+                "borderless": True,
+            }
+        ]
+
     @display(description=_('Created'))
     def display_created(self, instance: ProductBrand):
         return instance.created_at
@@ -193,8 +233,17 @@ class ProductBrandAdmin(ModelAdmin):
         queryset = queryset.order_by('name')
         return queryset, use_distinct
 
+class ProductInline(TabularInline):
+    model = Product
+    #fields = ['name',]
+    show_change_link = True
+    can_delete = True
+    tab = True
+    extra = 0
+
 @admin.register(ProductLine)
 class ProductLineAdmin(ModelAdmin):
+    inlines = [ProductInline,]
     add_fieldsets = (
         (
             _('Overview'), 
@@ -209,11 +258,12 @@ class ProductLineAdmin(ModelAdmin):
     )
     list_display = (
         'name',
-        'brand',
+        'display_brand_header',
         'display_created',
     )
     search_fields = (
         'name',
+        'brand__name',
     )
     list_filter = (
         'created_at',
@@ -246,8 +296,140 @@ class ProductLineAdmin(ModelAdmin):
         'created_at',
     )
 
+    @display(description=_('Name'), header=True)
+    def display_brand_header(self, instance: ProductLine):
+        """
+        Muestra la marca en la primera línea, el codigo en la segunda,
+        y un avatar en un círculo.
+        """
+        image_path = f"images/brands/{instance.brand.name.lower()}.jpg"
+        return [
+            instance.brand.name,
+            instance.brand.code,
+            None,
+            {
+                "path": static(image_path),
+                "squared": False,
+                "borderless": True,
+            }
+        ]
+
     @display(description=_('Created'))
     def display_created(self, instance: ProductLine):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('brand__name', 'name',)
+        for line in queryset:
+            line.name = f"{line.brand.name} - {line.name}"
+        return queryset, use_distinct
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: ProductSize):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('name')
+        return queryset, use_distinct
+
+@admin.register(ProductColor)
+class ProductColorAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: ProductColor):
         return instance.created_at
     
     def get_search_results(self, request, queryset, search_term):
@@ -262,12 +444,22 @@ class ProductAdmin(ModelAdmin):
             _('Overview'), 
             {
                 'fields': (
-                    'sku',
-                    'name',
-                    'size',
-                    'color',
-                    'line',
-                    'family',
+                    (
+                        'sku',
+                        'name',
+                    ),
+                    (
+                        'size',
+                        'color',
+                    ),
+                    (
+                        'line',
+                        'display_brand',
+                    ),
+                    (
+                        'family',
+                        'display_group',
+                    ),
                     'retail',
                 ),
                 'classes': ('wide',),
@@ -275,16 +467,20 @@ class ProductAdmin(ModelAdmin):
         ),
     )
     list_display = (
-        'sku',
-        'name',
-        'size',
-        'line',
-        'family',
-        'display_brand',
+        'display_product_header',
+        'display_brand_header',
+        'display_retail_header',
+        'display_coverage_header',
         'display_created',
     )
     search_fields = (
+        'sku',
         'name',
+        'size__name',
+        'color__name',
+        'family__name',
+        'line__name',   
+        'line__brand__name',
     )
     list_filter = (
         'created_at',
@@ -294,13 +490,24 @@ class ProductAdmin(ModelAdmin):
             _('Overview'),
             {
                 'fields': (
-                    'sku',
-                    'name',
-                    'size',
-                    'color',
-                    'line',
-                    'family',
+                    (
+                        'sku',
+                        'name',
+                    ),
+                    (
+                        'size',
+                        'color',
+                    ),
+                    (
+                        'line',
+                        'display_brand',
+                    ),
+                    (
+                        'family',
+                        'display_group',
+                    ),
                     'retail',
+                    'is_active',
                 ),
                 'classes': ['tab',],
             },
@@ -316,18 +523,88 @@ class ProductAdmin(ModelAdmin):
         ),
     )
     autocomplete_fields = (
-        'line',
+        'size',
+        'color',
         'family',
+        'line',    
         'retail',
     )
     readonly_fields = (
         'created_at',
         'display_brand',
+        'display_group',
     )
+
+    @display(description=_('Name'), header=True)
+    def display_product_header(self, instance: Product):
+        """
+        Muestra el nombre completo en la primera línea, el sku en la segunda,
+        y un avatar en un círculo.
+        """
+        return [
+            instance.full_name,
+            instance.sku,
+            None,
+            {
+                "path": static("images/products/dormitorio.jpg"),
+                "squared": False,
+                "borderless": True,
+            }
+        ]
+    
+    @display(description=_('Brand'), header=True)
+    def display_brand_header(self, instance: Product):
+        """
+        Muestra la marca en la primera línea, el codigo en la segunda,
+        y un avatar en un círculo.
+        """
+        image_path = f"images/brands/{instance.line.brand.name.lower()}.jpg"
+        return [
+            instance.line.brand.name,
+            instance.line.brand.code,
+            None,
+            {
+                "path": static(image_path),
+                "squared": False,
+                "borderless": True,
+            }
+        ]
+
+    @display(description=_('Brand'), header=True)
+    def display_retail_header(self, instance: Product):
+        """
+        Muestra el retail en la primera línea, el canal en la segunda,
+        y un avatar en un círculo.
+        """
+        image_path = f"images/retails/{instance.retail.name.lower()}.jpg"
+        return [
+            instance.retail.name,
+            instance.retail.channel.name,     
+            None,
+            {
+                "path": static(image_path),
+                "squared": False,
+                "borderless": True,
+            }
+        ]
+
+    @display(
+        description=_('Status'),
+        label={
+            _('inactive'): 'danger',
+            _('active'): 'success',
+        },
+    )
+    def display_coverage_header(self, instance: Product):
+        return _('active') if instance.is_active else _('inactive')
 
     @display(description=_('Brand'))
     def display_brand(self, instance: Product):
         return instance.line.brand
+    
+    @display(description=_('Group'))
+    def display_group(self, instance: Product):
+        return instance.family.group
 
     @display(description=_('Created'))
     def display_created(self, instance: Product):
@@ -337,4 +614,5 @@ class ProductAdmin(ModelAdmin):
         queryset, use_distinct = super().get_search_results(request, queryset, search_term)
         queryset = queryset.order_by('name')
         return queryset, use_distinct
+        
    
