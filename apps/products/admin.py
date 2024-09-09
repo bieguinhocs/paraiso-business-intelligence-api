5791e21da24be0b51d39a443b9a1e18333188ac5
biego <biegoe.lozano@gmail.com>
2024-09-08 19:00:42 -0500

    fead(admin): add ProductAdmin with customization

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,75 +1,15 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 from .models import (
-    ProductBrand,
     ProductGroup,
     ProductFamily,
+    ProductBrand,
     ProductLine,
     Product
 )
 from django.utils.translation import gettext_lazy as _
 from unfold.decorators import display
 from django.templatetags.static import static
-
-@admin.register(ProductBrand)
-class ProductBrandAdmin(ModelAdmin):
-    add_fieldsets = (
-        (
-            _('Overview'), 
-            {
-                'fields': (
-                    'code',
-                    'name',
-                ),
-                'classes': ('wide',),
-            },
-        ),
-    )
-    list_display = (
-        'code',
-        'name',
-        'display_created',
-    )
-    search_fields = (
-        'code',
-        'name',
-    )
-    list_filter = (
-        'created_at',
-    )
-    fieldsets = (
-        (
-            _('Overview'),
-            {
-                'fields': (
-                    'code',
-                    'name',
-                ),
-                'classes': ['tab',],
-            },
-        ),
-        (
-            _('Important dates'),
-            {
-                'fields': (
-                    'created_at',
-                ),
-                'classes': ['tab',],
-            },
-        ),
-    )
-    readonly_fields = (
-        'created_at',
-    )
-
-    @display(description=_('Created'))
-    def display_created(self, instance: ProductBrand):
-        return instance.created_at
-    
-    def get_search_results(self, request, queryset, search_term):
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        queryset = queryset.order_by('name')
-        return queryset, use_distinct
 
 @admin.register(ProductGroup)
 class ProductGroupAdmin(ModelAdmin):
@@ -193,6 +133,66 @@ class ProductFamilyAdmin(ModelAdmin):
         queryset = queryset.order_by('name')
         return queryset, use_distinct
 
+@admin.register(ProductBrand)
+class ProductBrandAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'code',
+                    'name',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'code',
+        'name',
+        'display_created',
+    )
+    search_fields = (
+        'code',
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'code',
+                    'name',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    readonly_fields = (
+        'created_at',
+    )
+
+    @display(description=_('Created'))
+    def display_created(self, instance: ProductBrand):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('name')
+        return queryset, use_distinct
+
 @admin.register(ProductLine)
 class ProductLineAdmin(ModelAdmin):
     add_fieldsets = (
@@ -248,6 +248,89 @@ class ProductLineAdmin(ModelAdmin):
 
     @display(description=_('Created'))
     def display_created(self, instance: ProductLine):
+        return instance.created_at
+    
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset = queryset.order_by('name')
+        return queryset, use_distinct
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    add_fieldsets = (
+        (
+            _('Overview'), 
+            {
+                'fields': (
+                    'sku',
+                    'name',
+                    'size',
+                    'color',
+                    'line',
+                    'family',
+                    'retail',
+                ),
+                'classes': ('wide',),
+            },
+        ),
+    )
+    list_display = (
+        'sku',
+        'name',
+        'size',
+        'line',
+        'family',
+        'display_brand',
+        'display_created',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'created_at',
+    )
+    fieldsets = (
+        (
+            _('Overview'),
+            {
+                'fields': (
+                    'sku',
+                    'name',
+                    'size',
+                    'color',
+                    'line',
+                    'family',
+                    'retail',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'created_at',
+                ),
+                'classes': ['tab',],
+            },
+        ),
+    )
+    autocomplete_fields = (
+        'line',
+        'family',
+        'retail',
+    )
+    readonly_fields = (
+        'created_at',
+        'display_brand',
+    )
+
+    @display(description=_('Brand'))
+    def display_brand(self, instance: Product):
+        return instance.line.brand
+
+    @display(description=_('Created'))
+    def display_created(self, instance: Product):
         return instance.created_at
     
     def get_search_results(self, request, queryset, search_term):
