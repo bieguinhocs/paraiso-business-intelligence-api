0c313dfb68c5b296b7c9be1a7bb7892fdde1436a
biego <biegoe.lozano@gmail.com>
2024-09-09 02:54:40 -0500

    feat(products): add some unique constraints

--- apps/products/models.py
+++ apps/products/models.py
@@ -64,13 +64,16 @@ class ProductBrand(models.Model):
         super().save(*args, **kwargs)
 
 class ProductLine(models.Model):
-    name = models.CharField(_('name'), max_length=255, unique=True)
+    name = models.CharField(_('name'), max_length=255)
     brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE, verbose_name=_('brand'))
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
         verbose_name = _('line')
         verbose_name_plural = _('lines')
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='nombre único', condition=~models.Q(name='-'))
+        ]
 
     def __str__(self):
         return self.name
@@ -119,26 +122,30 @@ class ProductColor(models.Model):
         super().save(*args, **kwargs)
 
 class Product(models.Model):
-    sku = models.CharField(_('SKU'), max_length=100, unique=True)
-    name = models.CharField(_('name'), max_length=255, unique=True)
+    sku = models.CharField(_('SKU'), max_length=100, blank=True, null=True)
+    name = models.CharField(_('name'), max_length=255)
     size = models.ForeignKey(ProductSize, on_delete=models.CASCADE, verbose_name=_('size'))
     color = models.ForeignKey(ProductColor, on_delete=models.CASCADE, blank=True, null=True, verbose_name=_('color'))
     line = models.ForeignKey(ProductLine, on_delete=models.CASCADE, verbose_name=_('line'))
     family = models.ForeignKey(ProductFamily, on_delete=models.CASCADE, verbose_name=_('family'))
-    retail = models.ForeignKey('stores.StoreRetail', on_delete=models.CASCADE, verbose_name=_('retail'))
+    retail = models.ForeignKey('stores.StoreRetail', on_delete=models.CASCADE, blank=True, null=True, verbose_name=_('retail'))
     is_active = models.BooleanField(_('status'), default=True, help_text=_('Indicates whether this product is active. Uncheck this option if it is not active.'))
+    description = models.TextField(_('description'), blank=True)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
     class Meta:
         verbose_name = _('product')
         verbose_name_plural = _('products')
+        constraints = [
+            models.UniqueConstraint(fields=['sku'], name='sku único', condition=models.Q(sku__isnull=False))
+        ]
 
     def __str__(self):
         return self.name
     
     @property
     def full_name(self):
-        line_val = f' {self.line}' if self.line else ''
+        line_val = f' {self.line.name}' if self.line.name != '-' else ''
         color_val = f' {self.color}' if self.color else ''
         return f"{self.family}{line_val} {self.name} {self.size}{color_val}"
     
