426424898baaf439343220c77d3bad99c004b1c1
biego <biegoe.lozano@gmail.com>
2024-12-03 15:13:15 -0500

    chore(settings): go back to debug=False

--- api/settings.py
+++ api/settings.py
@@ -30,7 +30,7 @@ sys.path.insert(0, str(BASE_DIR / 'apps'))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='fallback_default_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#'RENDER' not in os.environ
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -160,14 +160,14 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-## This production code might break development mode, so we check whether we're in DEBUG mode
-#if not DEBUG:
-#    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#    # and renames the files with unique names for each version to support long-term caching
-#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / "media"
 
