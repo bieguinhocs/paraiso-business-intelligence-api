7c6a633c65bb048f228169745550a89738b1dc73
biego <biegoe.lozano@gmail.com>
2024-12-14 02:58:03 -0500

    fix(personal-data): update verbose names

--- apps/personal_data/models.py
+++ apps/personal_data/models.py
@@ -7,16 +7,16 @@ from utils.validators import validate_only_digits, validate_document_number
 from utils.constants import DOCUMENT_TYPE_CHOICES
 
 class PersonalData(models.Model):
-    client_first_name = models.CharField(_('first name'), max_length=255)
-    client_last_name = models.CharField(_('last name'), max_length=255)
-    client_document_type = models.CharField(_('document type'), max_length=3, choices=DOCUMENT_TYPE_CHOICES)
-    client_document_number = models.CharField(_('document number'), max_length=9, unique=True)
-    client_phone = models.CharField(_('phone number'), max_length=9, validators=[validate_only_digits, MinLengthValidator(9)], blank=True, null=True)
-    client_email = models.EmailField(_('email'), blank=True, null=True)
+    client_first_name = models.CharField(_('client first name'), max_length=255)
+    client_last_name = models.CharField(_('client last name'), max_length=255)
+    client_document_type = models.CharField(_('client document type'), max_length=3, choices=DOCUMENT_TYPE_CHOICES)
+    client_document_number = models.CharField(_('client document number'), max_length=9, unique=True)
+    client_phone = models.CharField(_('client phone number'), max_length=9, validators=[validate_only_digits, MinLengthValidator(9)], blank=True, null=True)
+    client_email = models.EmailField(_('client email'), blank=True, null=True)
     answer_1 = models.TextField(_('answer 1'), blank=True, null=True)
     answer_2 = models.TextField(_('answer 2'), blank=True, null=True)
     photo = models.ImageField(
-        _('photo'),
+        _('survey photo'),
         upload_to='personal_data_survey_photos/',
         blank=False,
         null=False,
